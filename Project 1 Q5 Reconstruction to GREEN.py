6a5302287e3595871f0f82f13a298563bea074b3
likbalpande <60666678+likbalpande@users.noreply.github.com>
2021-11-12 22:57:49 +0530

    Create Project 1 Q5 Reconstruction to GREEN.py

---
+++ Project 1 Q5 Reconstruction to GREEN.py
@@ -0,0 +1,34 @@
+#Using CVXPY for Optimization Problem Solving
+import cvxpy as cpy
+#Using Numpy for solving equations
+import numpy as npy
+import scipy
+
+#loading the data
+C=npy.load("Data For Assignment GREEN/C.npy")
+y=npy.load("Data For Assignment GREEN/y.npy")
+A_inv=npy.load("Data For Assignment GREEN/A_inv.npy")
+
+#Reshaping the size of y as a single column vector
+y=y.reshape(y.shape[0],1)
+#Transforming s matrix as it needs to be valid for matrix multiplication
+s= cpy.Variable((C.shape[1],1))
+
+#Using CVXPY library for solving convex optimzation problem
+objective = cpy.Minimize(cpy.norm(s,1)); # objective function definition
+#constraint = [(y-C@s)**2==0] #given constraint
+constraint = [(y-C@s)==0] #this is similar as above constraint
+prob=cpy.Problem(objective,constraint) #Posed the problem
+
+result = prob.solve(solver=cpy.OSQP,verbose=True) 
+# result i:e an optimal solution (column vector will be obtained) verbose=True helped track the steps while the prob.solve was running
+
+#Solving the linear algebra equation to obtain x.
+x=scipy.linalg.solve(A_inv,s.value)#using the s
+
+print(x)
+print(s.value)
+
+#contents were saved
+npy.save("ans_s_green.npy",s)
+npy.save("ans_x_green.npy",x)
